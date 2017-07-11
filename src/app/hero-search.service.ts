eb4d35c7d5d27e7e6d29a30896a08d790004f7bb
CM\fmcarreroc <fmcarreroc@medilaser.co>
2017-07-11 07:46:33 -0500

    commit merge

--- src/app/hero-search.service.ts
+++ src/app/hero-search.service.ts
@@ -1,2 +1,12 @@
 import { Injectable }  from '@angular/core';
 import { Http } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import { Hero } from './hero';
+
+@Injectable
+export class HeroSearchService{
+
+    constructor(private http:Http){}
+    search(term :  string)
+}
